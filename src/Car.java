4e2b9dfb96dbdc6bf190ea78240172dadedf5777
unknown <asiam.rybak@gmail.com>
2019-11-24 13:02:19 +0100

    Moja pierwsza wiadomosc

---
+++ src/Car.java
@@ -0,0 +1,37 @@
+public class Car {
+    private String marka;
+    private String model;
+    private Integer rokProdukcji;
+    private Integer wiek;
+    private Integer predkosc;
+
+    Car (String marka, String model, Integer rok){
+        this.marka = marka;
+        this.model = model;
+        this.rokProdukcji = rok;
+        this.wiek = this.obliczWiek();
+        this.predkosc = 0;
+    }
+
+    private Integer obliczWiek(){
+        Integer aktualnyRok = 2019;
+        return aktualnyRok - rokProdukcji;
+    }
+    public void przyspiesz(Integer zmiana){
+        this.predkosc += zmiana;
+        System.out.println("Twoja aktualna prędkość to: " + this.predkosc);
+
+
+    }
+    public void opiszAuto(){
+        System.out.println("Oto " + this.marka + " " + this.model);
+        if (this.wiek < 5){
+            System.out.println("Nowe auto prawie nie używane");
+        }else if (this.wiek <=20){
+            System.out.println("Troche już jeździ, ale wciąż działa");
+        }else{
+            System.out.println("Nadaje sie tylko na zlom");
+        }
+    }
+
+}
